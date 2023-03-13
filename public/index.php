2d9dc2e839f454ac78a77c857f75562bdd618209
aliasst <ahtubafishing@yandex.ru>
2023-03-13 21:50:59 +0300

    UPD Request and Response

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
-use \Framework\Http\Request;
+
+use \Framework\Http\RequestFactory;
+use \Framework\Http\Response;
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-header('X-Developer: aliasst');
 
-$request = (new Request())->withQueryParams($_GET)->withParsedBody($_POST);
+
+$request = RequestFactory::fromGlobals();
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
+$response = (new Response('Hello, ' . $name))
+    ->withHeader('X-Developer', 'aliasst');
 
-echo 'Hello, ' . $name;
+header('HTTP/1.0 '. $response->getStatusCode(). ' ' . $response->getReasonPhrase());
+foreach($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+echo $response->getBody();
+
